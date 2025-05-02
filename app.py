19dd2a368642016f5060639fc704f660f6f9ede4
tsrira <36183515+tsrira@users.noreply.github.com>
2025-05-02 10:08:30 +0530

    Update app.py
    xgboost

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import xgboost as xgb  # Importing xgboost
 
 # Load the model
 with open('xgboost_classification.pkl', 'rb') as f:
